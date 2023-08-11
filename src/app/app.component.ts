c810e36b297b221560b5db2c9046d6c64d20cab7
Prashant Kumar <prashant.ginims@gmail.com>
2023-08-11 19:17:31 +0530

    add confetti after app loads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {
   Router,
   RoutesRecognized,
 } from '@angular/router';
+import { ConfettiService } from './core/services/confetti/confetti.service';
 
 @Component({
   selector: 'app-root',
@@ -26,7 +27,10 @@ export class AppComponent {
   bufferValue: number = 10;
   isLoading: boolean = true;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private confettiService: ConfettiService
+  ) {
     this.subscribeRouterEvent();
   }
 
@@ -60,5 +64,9 @@ export class AppComponent {
   private setProgressBar(value: number, bufferValue: number) {
     this.value = value;
     this.bufferValue = bufferValue;
+  }
+
+  ngAfterViewInit(): void {
+    this.confettiService.fireWorksOnLoad();
   }
 }
